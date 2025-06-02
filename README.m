190d4a74be5c656e87b63b7ccd5e48b64aa23a0a
MuritalaQudus <145025011+MuritalaQudus@users.noreply.github.com>
2025-06-02 13:26:08 +0100

    Create README.md

---
+++ README.m
@@ -0,0 +1,28 @@
+Body Fat Percentage Predictor
+This is a simple web application built with Streamlit that predicts a user's estimated body fat percentage based on daily health metrics such as weight, calorie intake, protein intake, sleep duration, and steps walked.
+
+🔍 Project Overview
+The app uses a machine learning model trained on a mock dataset simulating 90 days of health and fitness data. It demonstrates how regression models can be applied in fitness-related predictions.
+
+click to Launch the App https://muritalaqudus-body-fat-app-app-iml736.streamlit.app/
+
+Features
+
+Predicts body fat percentage based on 5 input features:
+
+Weight (kg)
+
+Calories
+
+Protein (g)
+
+Sleep (hrs)
+
+Steps walked
+
+Clean and interactive user interface
+
+Built with Streamlit and scikit-learn
+
+Trained on a mock fitness dataset
+
